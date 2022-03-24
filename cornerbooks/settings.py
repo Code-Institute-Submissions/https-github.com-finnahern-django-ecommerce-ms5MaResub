0c1f7946a0f4c454468c47b01bdd2a06ed4c4486
finnahern <finna9x@gmail.com>
2022-03-24 17:09:17 +0000

    Complete desktop header in base.html

--- cornerbooks/settings.py
+++ cornerbooks/settings.py
@@ -144,6 +144,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # AWS bucket config
 if 'USE_AWS' in os.environ:
