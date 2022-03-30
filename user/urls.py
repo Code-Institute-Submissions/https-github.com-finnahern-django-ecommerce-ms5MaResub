7a82330511599d447951f56fdafa5c70522dec14
finnahern <finna9x@gmail.com>
2022-03-30 21:09:52 +0000

    Add logout function

--- user/urls.py
+++ user/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path("", views.blog_list, name="blog"),
     path("login/", views.user_login, name="login"),
     path("register/", views.register, name="register"),
 ]
