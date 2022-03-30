c82f6ba2c1af91c43449c2eb09ef2e84baf0d09c
finnahern <finna9x@gmail.com>
2022-03-30 13:17:44 +0000

    Add blog_post template, views and urls so users can click on a blog post and see it's full text

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.blog_list, name="blog"),
+    path("<int:year>/<int:month>/<int:day>/<slug:post>/", views.blog_post, name="blog_post"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 
@@ -11,6 +11,24 @@ def blog_list(request):
     template = "blog/blog.html"
     context = {
         "posts": posts
+    }
+
+    return render(request, template, context)
+
+
+def blog_post(request, year, month, day, post):
+    """
+    A view to render an individual page for each
+    blog post in the database.
+    """
+
+    post = get_object_or_404(Post, slug=post,
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
+    template = "blog/blog_post.html"
+    context = {
+        "post": post
     }
 
     return render(request, template, context)
